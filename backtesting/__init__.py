ef157fc0da2ecd7f88936ded4cafc5d59c912c36
carlos <carlos.o.bain@gmail.com>
2026-01-30 18:23:49 +0000

    .

---
+++ backtesting/__init__.py
@@ -0,0 +1,10 @@
+"""
+Backtesting module for the Stock Agent.
+
+This module provides backtesting functionality for the ReasoningAgent,
+allowing historical simulation of trading decisions.
+"""
+
+from backtesting.run_backtest import run_backtest
+
+__all__ = ["run_backtest"]
